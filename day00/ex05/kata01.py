ca19a41bd51a50b572e6fb16f06bac5cab6644e5
manaccac <maximenaccache@gmail.com>
2020-05-25 15:47:32 +0200

    kata01

---
+++ day00/ex05/kata01.py
@@ -0,0 +1,13 @@
+languages = {
+'Python': 'Guido van Rossum',
+'Ruby': 'Yukihiro Matsumoto',
+'PHP': 'Rasmus Lerdorf',
+}
+
+i = 0
+keys = list(languages.keys())
+values = list(languages.values())
+
+while (i != len(keys)):
+    print(keys[i], "was created by", values[i])
+    i += 1
