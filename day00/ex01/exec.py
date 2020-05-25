653e6dc37c0fba694224d4f25a51063403fead07
manaccac <maximenaccache@gmail.com>
2020-05-25 13:58:14 +0200

    ex01 fini

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -6,7 +6,7 @@
 #    By: manaccac <manaccac@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/05/25 08:44:39 by manaccac          #+#    #+#              #
-#    Updated: 2020/05/25 08:44:41 by manaccac         ###   ########lyon.fr    #
+#    Updated: 2020/05/25 13:57:31 by manaccac         ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,13 +14,14 @@ import sys
 
 argc = len(sys.argv)
 i = argc - 1
-while (i != 0):
-    str = sys.argv[i]
-    str_len = len(str)
-    res = str[::-1]
-    res = res.swapcase()
-    i = i - 1
-    if (i == 0):
-        print (res, end='')
-    else:
-        print (res, end=' ')
+if (argc > 2):
+    while (i != 0):
+        str = sys.argv[i]
+        str_len = len(str)
+        res = str[::-1]
+        res = res.swapcase()
+        i = i - 1
+        if (i == 0):
+            print (res, end='')
+        else:
+            print (res, end=' ')
