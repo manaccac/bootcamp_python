99d04db3400b8bb12149d0a107939367f6eaddab
manaccac <maximenaccache@gmail.com>
2020-05-26 09:01:31 +0200

    ex02 norminette

--- day00/ex02/whois.py
+++ day00/ex02/whois.py
@@ -1,16 +1,5 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    whois.py                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: manaccac <manaccac@student.42lyon.fr>      +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/05/25 09:19:14 by manaccac          #+#    #+#              #
-#    Updated: 2020/05/25 09:22:29 by manaccac         ###   ########lyon.fr    #
-#                                                                              #
-# **************************************************************************** #
-
 import sys
+
 
 argc = len(sys.argv)
 if (argc == 1):
@@ -22,14 +11,14 @@ nb = sys.argv[1]
 if (nb.isdigit()):
     nb = int(nb)
     if (nb == 0):
-        print ("I'm Zero.")
+        print("I'm Zero.")
         exit()
     if (nb % 2 == 0):
-        print ("I'm Even.")
+        print("I'm Even.")
         exit()
     else:
-        print ("I'm Odd.")
+        print("I'm Odd.")
         exit()
 else:
-    print ("ERROR")
+    print("ERROR")
     exit()
